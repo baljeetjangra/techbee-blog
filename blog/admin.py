0915a01bd957eb29b253043ca2a5857be2e24162
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-06-04 17:26:39 +0530

    blog model created

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
-
+from .models import Post
 # Register your models here.
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','created_on','updated_on']
+    list_filter = ['created_on','updated_on']
+    search_fields = ['title','content']
+    prepopulated_fields = {'slug':('title',)}
+    class Meta:
+        model = Post
+admin.site.register(Post,PostAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now=False,auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True,auto_now_add=False)
+
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"slug": self.slug})
+    

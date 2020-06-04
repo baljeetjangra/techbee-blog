d8be39eb21bc8bb18e3a565d8ca7ebd8a2aec43a
Baljeet Jangra <baljeetjangra45@gmail.com>
2020-06-04 16:32:17 +0530

    project setup

---
+++ techbee/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/media/baljeet/Projects/My Projects/techbeeblog/techbee/techbee/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()

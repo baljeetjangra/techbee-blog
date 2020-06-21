50d46122c28931258483794d29771cb37724a8da
Baljeet jangra <baljeetjangra45@gmail.com>
2020-06-21 16:44:08 +0530

    similar posts suggestion functionality added

--- blog/views.py
+++ blog/views.py
@@ -5,6 +5,7 @@ from django.core.paginator import Paginator, EmptyPage,PageNotAnInteger
 # from django.views.generic import ListView
 from django.core.mail import send_mail
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 
 #  ++++++++++++++++++++++Class based post list view+++++++++++++++
@@ -51,7 +52,11 @@ def post_detail(request,year,month,day,post):
             new_comment.save()
     else:
         comment_form = CommentForm()
-    return render(request,'blog/post/detail.html',{'post':post,'new_comment':new_comment,'comment_form':comment_form,'comments':comments})
+    
+    post_tags_ids = post.tags.values_list('id',flat=True)
+    similar_posts = post.published.filter(tags__in=post_tags_ids).exclude(id=post.id)
+    similar_posts =similar_posts.annotate(same_tags=Count('tags')).order_by('-same_tags','-publish')[:4]
+    return render(request,'blog/post/detail.html',{'post':post,'new_comment':new_comment,'comment_form':comment_form,'comments':comments,'similar_posts': similar_posts})
 
 
 def post_share(request,post_id):

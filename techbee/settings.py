ba4f2bd0ffa198e5394ff9cb80441c8e0d651799
Baljeet jangra <baljeetjangra45@gmail.com>
2020-06-25 20:09:00 +0530

    Frontend Changes - Comments

--- techbee/settings.py
+++ techbee/settings.py
@@ -43,7 +43,11 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'django.contrib.sitemaps',
     'django.contrib.postgres',
+    'crispy_forms',
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
